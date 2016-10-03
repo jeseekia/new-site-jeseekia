5ace64df33fdabf13d9b86e6188c35c2d67a04b9
Jeseekia Vaughn <jeseekia@gmail.com>
2016-10-03 17:54:51 -0400

    Testing out the Materialize scrollfire plugin. Not currently working.

--- js/init.js
+++ js/init.js
@@ -3,5 +3,16 @@
 
     $('.button-collapse').sideNav();
 
+    var options = [
+    // {selector: '.class', offset: 200, callback: customCallbackFunc } },
+    // {selector: '.other-class', offset: 200, callback: function() {
+    //   customCallbackFunc();
+    // } },
+    {selector: '#staggered-portfolio-list', offset: 500, callback: function(el) {
+      Materialize.showStaggeredList($(el));
+    } },
+  ];
+  Materialize.scrollFire(options);
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
